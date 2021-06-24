597233ddc8ca6401c8fcff61c823e370411fddc0
ajarek <ajarek@poczta.onet.pl>
2021-06-25 00:16:19 +0200

    change js

--- script.js
+++ script.js
@@ -1,10 +1,62 @@
 const squares = document.querySelectorAll('.square')
 
-let snake = Math.floor(Math.random() * squares.length)
+let snake =[]
+snake.push(Math.floor(Math.random() * squares.length))
+const start=()=>{
+    snake.forEach(el=>{
+       squares[el].classList.add('snake')
+       })
+    }
+    start()
 let rund = 0
 while (rund < 8) {
     let apple = Math.floor(Math.random() * squares.length)
-    squares[apple].style.backgroundImage = `url('apple.png')`
+    squares[apple].classList.add('apple')
     rund++
 }
-squares[snake].style.backgroundImage = `url('snake.jpg')`
+
+const colizion=()=>{
+    squares.forEach((square,index)=>{
+        snake.forEach(elem=>{
+        if(square.classList.contains('apple') && index===elem){
+           console.log(snake.length);
+            square.classList.remove('apple')
+            
+            snake.push(snake[0]+snake.length)
+            console.log(snake);
+        }
+    })
+    }) 
+    
+ }
+ 
+
+const control = (e) => {
+   e.preventDefault()
+   
+    for(i=0;i<snake.length;i++){
+    squares[snake[i]].classList.remove('snake')
+    if(e.keyCode===39){
+        squares[snake[i]+1].classList.add('snake')
+         snake[i]=snake[i]+1
+    }//right
+    else if(e.keyCode===38){
+        squares[snake[i]-15].classList.add('snake')
+        snake[i]=snake[i]-15
+    }//up
+    else if(e.keyCode===37){
+        squares[snake[i]-1].classList.add('snake')
+        snake[i]=snake[i]-1
+    }//left
+    else if(e.keyCode===40){
+        squares[snake[i]+15].classList.add('snake')
+        snake[i]=snake[i]+15
+    }//down
+    
+}
+
+colizion()
+start()
+}
+document.addEventListener('keyup',control)
+
